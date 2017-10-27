d7f59e542b1c32c7893030c648ccaf7ab019df88
jayanga vithana <jayanga38@gmail.com>
2017-10-26 22:21:41 -0500

    setup completed successfully

---
+++ sacbackend/src/main/java/jay/first/sacbackend/App.java
@@ -0,0 +1,13 @@
+package jay.first.sacbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
